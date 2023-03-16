66035bcb8cf7f75fde9d5900c2900d79dc7e360d
mamalovesmeat <chebetd75a@gmail.com>
2023-03-16 12:47:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+
+/**
+ * main - controls program exexcution
+ *
+ * Return: 0 when successfull
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
