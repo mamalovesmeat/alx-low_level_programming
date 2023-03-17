51339e5d3713705b5e8d651283b2a194de60e0f7
mamalovesmeat <chebetd75a@gmail.com>
2023-03-17 15:17:51 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -10,10 +10,13 @@ int main(void)
 	int n;
 
 	for (n = 48; n < 58; n++)
+
 		putchar(n);
 		if (n != 57)
+
 			putchar(',');
 			putchar(' ');
+
 
 	putchar('\n');
 	return (0);
