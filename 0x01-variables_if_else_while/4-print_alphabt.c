48e315f7c30d67b4c1277a60ef24087123ee3fb8
mamalovesmeat <chebetd75a@gmail.com>
2023-03-17 14:04:33 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib>
+#include<string.h>
+
+/**
+ * main - prints the aplhabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */int main(void)
+{
+	int i;
+
+	for (i = 97; i <123; i++)
+		if ( i != 101 && i != 113)
+			putchar(i);
+
+	putchar('\n');
+	return (0);
+}
